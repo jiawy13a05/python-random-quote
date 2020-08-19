c35d04f931d2ae28bd98a87435219a220ca70bb1
jiawenyao <jiawenyao401@sina.com>
2020-08-19 16:02:18 +0800

    run quote mothod

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,12 @@
 def a():
   print("Keep it logically awesome.1111")
   
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  print(quotes)
+  print(quotes[0])
 
 if __name__== "__main__":
   a()
