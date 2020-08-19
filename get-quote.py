28df467b604277e6ccb62bc1a7827265d18bb2c0
jiawenyao <jiawenyao401@sina.com>
2020-08-19 16:11:43 +0800

    import random count

--- get-quote.py
+++ get-quote.py
@@ -1,3 +1,4 @@
+import random
 def a():
   print("Keep it logically awesome.1111")
   
@@ -5,8 +6,13 @@ def a():
   quotes = f.readlines()
   f.close()
 
-  print(quotes)
+  #print(quotes)
   print(quotes[0])
+  print(quotes[-1])
+  print(quotes[len(quotes)-1])
+  last = len(quotes)-1
+  rnd = random.randint(0,last)
+  print(quotes[rnd])
 
 if __name__== "__main__":
   a()
